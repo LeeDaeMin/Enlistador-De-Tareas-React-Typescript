dcb81b2a0c50a5f44d6efb924277043d3c685a39
LeeDaeMin <laalmeida@uts.edu.co>
2023-03-05 23:48:48 -0500

    creation logic of component Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+
+export const Footer: React.FC<Props> = ({
+  activeCount,
+  todos,
+  onClearCompleted
+}) => {
+  return (
+        <footer className="footer">
+            <span className="todo-count">
+                <strong>{todos.length}</strong> Items
+            </span>
+
+            <Filters
+                filterSelect={}
+                onFilterCheck={()=>{
+
+                }}
+                />
+        </footer>
+  )
+}
